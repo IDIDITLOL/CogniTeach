b81948a4f52eb0ea7a239c066057c52823013876
IDIDITLOL <77718267+IDIDITLOL@users.noreply.github.com>
2023-12-29 17:57:16 +0000

    basic files upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import (Flask, redirect, render_template, request,send_from_directory, url_for)
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return "this is something <h1>It works</h1>"
+@app.route("/lol")
+def lol():
+    return render_template("imalive.html")
+if __name__=="__main__":
+    app.run()
