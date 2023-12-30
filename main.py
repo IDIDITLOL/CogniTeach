587781eec8487402973387acf9579a8e7605cd7f
IDIDITLOL <77718267+IDIDITLOL@users.noreply.github.com>
2023-12-30 08:34:42 +0000

    try to fix 404 error

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return "this is something <h1>It works</h1>"
-@app.route("/lol")
+@app.route("/siha")
 def lol():
+    return render_template("imalive.html")
+@app.route("why")
+def hi():
     return render_template("imalive.html")
 if __name__=="__main__":
     app.run()
